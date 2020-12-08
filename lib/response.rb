3fcec5972f7a721eb1056cc338853ae690fe2924
Max Mitrani <mjmitrani168@gmail.com>
2020-12-08 10:30:02 -0700

    add concept code for RespPrp

--- lib/response.rb
+++ lib/response.rb
@@ -1,1 +1,12 @@
+class RespPrep
 SERVER_ROOT = "/ts_challenge_2/"
+
+def make_response
+    if request.fetch(:path) == '/people'
+        #read from people controller method index
+    elsif request.fetch(:path) == '/person'
+        #read from people controller method index
+    else
+        #respond with errors
+    end
+end
