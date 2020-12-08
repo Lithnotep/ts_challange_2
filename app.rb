bebd136dc40def7f186a9caea66c0bd5a577934d
Max Mitrani <mjmitrani168@gmail.com>
2020-12-08 11:48:26 -0700

    add functional json response

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ server  = TCPServer.new('localhost', 8080)
 
 loop {
   client  = server.accept
-  request = client.readpartial(2048)
-  parse = Parser.new.parse(request)
+  request = Parser.new.parse(client.readpartial(2048))
+  names = {"data": [["mitrani","max"]]}
+  response = Response.new(code: 200, data: names.to_json)
+  response.send(client)
 }
 
--- lib/response.rb
+++ lib/response.rb
@@ -1,37 +1,37 @@
-class RespPrep
-SERVER_ROOT = "/ts_challenge_2/"
+# class RespPrep
+#     # SERVER_ROOT = "/ts_challenge_2/"
 
-    def confirm_response(request)
-        if request.fetch(:path) == '/people'
-            #read from people controller method index
-        elsif request.fetch(:path) == '/person'
-            #read from people controller method index
-        else
-            #respond with errors
-        end
-    end
+#     # def confirm_response(request)
+#     #     if request.fetch(:path) == '/people'
+#     #         #read from people controller method index
+#     #     elsif request.fetch(:path) == '/person'
+#     #         #read from people controller method index
+#     #     else
+#     #         #respond with errors
+#     #     end
+#     # end
 
-    def send_ok_response(data)
-        #responese with json data
-    end
+#     # def send_ok_response(data)
+#     #     #responese with json data
+#     # end
   
-    def send_file_not_found
-        #response with error code
-    end
-end
+#     # def send_file_not_found
+#     #     #response with error code
+#     # end
+# end
 
 class Response
     
-    def initialize(code, data: '')
+    def initialize(code:, data: '')
         @response = 
         "HTTP/1.1 #{code}\r\n" +
         "Content-type: application/json\r\n" +
         "\r\n" +
-        "#{data}\r\n"
+        data
     end
 
     def send(client)
-        client.write(@responsee)
+        client.write(@response)
     end
 end
 
